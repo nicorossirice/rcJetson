a748428d02cea54f9ee08a923a94ce8769199587
Nico Rossi <ncr3@rice.edu>
2022-11-29 21:57:12 -0600

    recordData ready for initial test

---
+++ recordData.py
@@ -0,0 +1,33 @@
+import signal
+
+import cv2
+
+from EthernetAPI.client import Client
+from EthernetAPI.message_types import RC_DATA
+
+def cleanup(signum, stackframe):
+    print("\nCleaning up...\n")
+    #client.disconnect()
+    exit()
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, cleanup)
+
+    client = Client()
+    client.connect("192.168.137.1", 60006)
+
+    old_mask = signal.pthread_sigmask({signal.SIGINT}, signal.SIG_BLOCK)
+
+    while True:
+        messages = client.read_messages()
+        if messages:
+            for mtype, message in messages:
+                if mtype == RC_DATA:
+                    throttle, steering = message.splot("|")
+                    print(f"{throttle}|{steering}")
+
+        if signal.SIGINT in signal.sigpending():
+            client.disconnect()
+            break
+
+        signal.pthread_sigmask(old_mask, signal.SET_MASK)
